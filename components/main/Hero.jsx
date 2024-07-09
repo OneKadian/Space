1197f9c2fbc83934c2e14d3dc8883dfb3c4d12f6
Anirudh Kadian <coderanirudh@gmail.com>
2024-07-09 18:37:39 +0530

    made some hero section changes

--- components/main/Hero.jsx
+++ components/main/Hero.jsx
@@ -33,19 +33,21 @@ export const Hero = () => {
             className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]]"
           >
             <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-            <h1 className="Welcome-text text-[13px]">Anirudh Kadian</h1>
+            <h1 className="Welcome-text text-[13px]">
+              "Turns projects into products" - Brian
+            </h1>
           </motion.div>
 
           <motion.div
             variants={slideInFromLeft(0.5)}
-            className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[480px] w-auto h-auto lg:max-w-[600px]"
+            className="flex flex-col gap-6 mt-6 text-5xl text-bold text-white max-w-[480px] w-auto h-auto lg:max-w-[600px]"
           >
             <span>
-              Providing{" "}
+              Launch your{" "}
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-                the best
+                SaaS
               </span>{" "}
-              project experience.
+              in the next 30 Minutes.
             </span>
           </motion.div>
 
@@ -53,21 +55,21 @@ export const Hero = () => {
             variants={slideInFromLeft(0.8)}
             className="text-lg text-gray-400 my-5 max-w-[600px]"
           >
-            I&apos;m a Full Stack Software Engineer with experience in Website,
-            Mobile, and Software development. Check out my projects and skills.
+            Code Boilerplate, resource sheet and templates to turn developers
+            into founders and help them launch instantly
           </motion.p>
 
           <motion.a
             variants={slideInFromLeft(1)}
             className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
           >
-            Learn more
+            Launch Now
           </motion.a>
         </div>
 
         <motion.div
           variants={slideInFromRight(0.8)}
-          className="w-full h-full flex justify-center items-center"
+          className="w-full h-full justify-center items-center hidden lg:flex"
         >
           <Image
             src="/hero-bg.svg"
